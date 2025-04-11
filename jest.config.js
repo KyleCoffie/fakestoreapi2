0cf17c8da1437e4b104377ce7bdd330975bb4f8e
Kyle Coffie <Kyle.coffie@live.com>
2025-04-10 19:06:06 -0500

    ci-cd update

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,5 @@
-export default {
+module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
-    moduleDirectories:['node_modules','src']
-    transform: {
-      '^.+\\.tsx?$': 'ts-jest',
-    },
-    moduleNameMapper: {
-      // Mock CSS and other style files
-      '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-
-      // Mock image and asset files
-      '\\.(jpg|jpeg|png|gif|webp|svg|eot|otf|ttf|woff|woff2)$': '<rootDir>/__mocks__/fileMock.js',
-    },
-    setupFiles: ['./jest.setup.ts'],
-  };
+    moduleDirectories:['node_modules','src'],
+    };
