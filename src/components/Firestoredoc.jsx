e04941317043c23c2d58c6db0144c3ae89027449
Kyle Coffie <Kyle.coffie@live.com>
2025-04-16 19:23:41 -0500

    cleaned up some files problem solving errors

--- src/components/Firestoredoc.jsx
+++ src/components/Firestoredoc.jsx
@@ -1,8 +1,8 @@
 import { getFirestore, doc, setDoc } from "firebase/firestore";
 
-const db = getFirestore();
-const userRef = doc(db, "users", user.uid);
-await setDoc(userRef, {
+const db = getFirestore(); //called to retrieve the database instance. This instance (db) is then used for any subsequent database operations.
+const userRef = doc(db, "users", user.uid); //This line creates a reference to a document within the "users" collection. The document's identifier is derived from user.uid, which uniquely identifies the user. If a document with this ID does not already exist, Firestore will create one when data is written.
+await setDoc(userRef, { //function is used to write an object to the document.
   email: user.email,
   createdAt: new Date(),
 });
