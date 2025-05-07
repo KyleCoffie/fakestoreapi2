4da5f3c459daed5583d1b7a412d0acf4eaf478a8
Kyle Coffie <Kyle.coffie@live.com>
2025-05-07 16:18:27 -0500

    x's6

--- jest.config.cjs
+++ jest.config.cjs
@@ -5,12 +5,6 @@ module.exports = {
     setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'], // Add setup file for polyfills
     moduleNameMapper: {
       '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    },
-    "transform": {
-        "^.+\\.m?[jt]sx?$": "jest-esm-transformer"
-      }
-    ,
-    
-      "extensionsToTreatAsEsm": [".ts"]
+    }
 };
    
